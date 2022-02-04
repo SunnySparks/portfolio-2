94401d85724e10724fe0636667809f05aeecabdc
SunnySparks <Josediazt6@outlook.com>
2022-02-03 21:26:43 -0600

    Add background image

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,8 @@
 const Home = () => (
-  <div>Home</div>
+  <div>
+    Home
+    <div>Illustration by <a href="https://icons8.com/illustrations/author/60312013123f993d795ce208">Oleg Shcherba</a> from <a href="https://icons8.com/illustrations">Ouch!</a></div>
+  </div>
 );
 
 export default Home;

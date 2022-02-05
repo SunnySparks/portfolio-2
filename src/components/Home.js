46ca6e4058ad777de64d3d3127b9a801f9cfa52a
SunnySparks <Josediazt6@outlook.com>
2022-02-04 19:00:14 -0600

    Add text for portfolio display

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,13 @@
 import './home.css';
 
 const Home = () => (
-  <div className="bg">
-    Home
+  <div className="bg d-flex flex-row">
+    <div className="left">
+      <div className="display-container">
+        <h1 class="display-1 display-font">Sunny Diaz</h1>
+      </div>
+    </div>
+    <div className="right"></div>
     <div>Illustration by <a href="https://icons8.com/illustrations/author/60312013123f993d795ce208">Oleg Shcherba</a> from <a href="https://icons8.com/illustrations">Ouch!</a></div>
   </div>
 );

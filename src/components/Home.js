fabac503cbd2a9b2f916755c1c99817ba2e1c1b0
SunnySparks <Josediazt6@outlook.com>
2022-02-10 20:41:46 -0600

    Add social networks to website

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,22 @@
 import './home.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebookF, faGithubSquare, faInstagramSquare } from '@fortawesome/free-brands-svg-icons' 
 
 const Home = () => (
   <div className="bg d-flex flex-row">
     <div className="left">
       <div className="display-container">
         <h1 class="display-1 display-font">Sunny Diaz</h1>
+        <div>
+          <p>
+            Hi There! My name is Sunny Díaz. I am a web developer and designer from Oaxaca, México.
+            I love creating websites and work on the front-end and UI.
+          </p>
+          <h1>
+            <a className="icon-link px-2" id="git-h" href="https://github.com/sunnySparks" ><FontAwesomeIcon icon={faGithubSquare} /></a>
+            <a className="icon-link px-2" id="insta" href="https://www.instagram.com/webdev.sunnydiaz/"><FontAwesomeIcon icon={faInstagramSquare} /></a>
+          </h1>
+        </div>
       </div>
     </div>
     <div className="right"></div>

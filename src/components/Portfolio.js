cee0a5286b52eaaa8e14e03e284c7d24adfae6bc
SunnySparks <Josediazt6@outlook.com>
2022-02-15 17:14:23 -0600

    Style portfolio page

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,5 +1,27 @@
+import './portfolio.css';
+
 const Portfolio = () => (
-  <div>Portfolio</div>
+  <div className="bg-img">
+    <div>Portfolio</div>
+    <div>
+      <h1>Front-End Projects</h1>
+      <ul>
+        <li>
+          <h3>ACNH Metrics</h3>
+          <div></div>
+          <div>
+            <p>This was my ReactJS Capstone project. The purpose of this project was to build an entire react application that consumed data from the Animal Crossing: New horizons API. This application consists of 3 different pages: Villagers, Fossils and Art</p>
+          </div>
+        </li>
+      </ul>
+    </div>
+    <div>
+      <h1>Full-Stack and Back-End Projects</h1>
+      <ul>
+        <li></li>
+      </ul>
+    </div>
+  </div>
 );
 
 export default Portfolio;

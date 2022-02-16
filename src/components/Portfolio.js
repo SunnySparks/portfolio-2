94be5d3d92415d2e248ef8a04602f5db9b342ffe
SunnySparks <Josediazt6@outlook.com>
2022-02-16 16:58:53 -0600

    Add second project

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -2,11 +2,12 @@ import './portfolio.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHtml5, faCss3Alt, faJs, faReact } from '@fortawesome/free-brands-svg-icons' 
 import acnh from './media/acnh.png';
+import space from './media/space.png';
 
 const Portfolio = () => (
   <div className="bg-img pt-5">
     <div className="pt-2">
-      <h1 className="gamer-font">Front-End Projects</h1>
+      <h1 className="gamer-font text-white">Front-End Projects</h1>
       <ul>
         <li className="project-cont m-3 p-3">
           <h3>ACNH Metrics</h3>
@@ -20,7 +21,24 @@ const Portfolio = () => (
                 <h2><FontAwesomeIcon icon={faCss3Alt} /></h2>
                 <h2><FontAwesomeIcon icon={faJs} /></h2>
                 <h2><FontAwesomeIcon icon={faReact} /></h2>
-                <button class="button button--telesto"><span><span>See the project</span></span></button>
+                <a href="https://github.com/SunnySparks/acnh-metrics"><button class="button button--telesto"><span><span>See the project</span></span></button></a>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li className="project-cont m-3 p-3">
+          <h3>Spaceships App</h3>
+          <div className="d-flex flex-row text-center align-items-center">
+            <img src={space} className='img-container'></img>
+            <div className="project-text mx-3 p-2">
+              <p>React-Redux Spaceship web application thatpulls data from the SpaceX API, Users can book their favorite rockets and space missions, and they can see it from their profiles</p>
+              <div className="ml-auto mr-auto">
+                <p>Built With:</p>
+                <h2><FontAwesomeIcon icon={faHtml5} /></h2>
+                <h2><FontAwesomeIcon icon={faCss3Alt} /></h2>
+                <h2><FontAwesomeIcon icon={faJs} /></h2>
+                <h2><FontAwesomeIcon icon={faReact} /></h2>
+                <a href="https://github.com/SunnySparks/spaceships"><button class="button button--telesto"><span><span>See the project</span></span></button></a>
               </div>
             </div>
           </div>
@@ -28,7 +46,7 @@ const Portfolio = () => (
       </ul>
     </div>
     <div>
-      <h1 className="gamer-font">Full-Stack and Back-End Projects</h1>
+      <h1 className="gamer-font text-white">Full-Stack and Back-End Projects</h1>
       <ul>
         <li></li>
       </ul>

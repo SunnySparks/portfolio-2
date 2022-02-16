f726102ab7960d4edb902d20b2372870927be4c8
SunnySparks <Josediazt6@outlook.com>
2022-02-16 17:23:07 -0600

    Adjust background proportion for element repetitions

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -5,7 +5,7 @@ import acnh from './media/acnh.png';
 import space from './media/space.png';
 
 const Portfolio = () => (
-  <div className="bg-img pt-5">
+  <div className="bg-pink pt-5">
     <div className="pt-2">
       <h1 className="gamer-font text-white">Front-End Projects</h1>
       <ul>
@@ -22,6 +22,23 @@ const Portfolio = () => (
                 <h2><FontAwesomeIcon icon={faJs} /></h2>
                 <h2><FontAwesomeIcon icon={faReact} /></h2>
                 <a href="https://github.com/SunnySparks/acnh-metrics"><button class="button button--telesto"><span><span>See the project</span></span></button></a>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li className="project-cont m-3 p-3">
+          <h3>Spaceships App</h3>
+          <div className="d-flex flex-row text-center align-items-center">
+            <img src={space} className='img-container'></img>
+            <div className="project-text mx-3 p-2">
+              <p>React-Redux Spaceship web application thatpulls data from the SpaceX API, Users can book their favorite rockets and space missions, and they can see it from their profiles</p>
+              <div className="ml-auto mr-auto">
+                <p>Built With:</p>
+                <h2><FontAwesomeIcon icon={faHtml5} /></h2>
+                <h2><FontAwesomeIcon icon={faCss3Alt} /></h2>
+                <h2><FontAwesomeIcon icon={faJs} /></h2>
+                <h2><FontAwesomeIcon icon={faReact} /></h2>
+                <a href="https://github.com/SunnySparks/spaceships"><button class="button button--telesto"><span><span>See the project</span></span></button></a>
               </div>
             </div>
           </div>
